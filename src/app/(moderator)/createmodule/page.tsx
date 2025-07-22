2f537adfd3eda9f8ffb4c1e97ba2a8cddd38e143
shubhamrajpandey <shubhamrajpandey875@gmail.com>
2025-07-22 20:55:47 +0545

    fix: module and home section

---
+++ src/app/(moderator)/createmodule/page.tsx
@@ -0,0 +1,13 @@
+import LevelDropdown from "@/app/components/Dropdown/LevelDropdown";
+import SubjectDropdown from "@/app/components/Dropdown/SubjectDropdown";
+
+import React from 'react'
+
+export const CreateModule = () => {
+  return (
+    <div>
+         <SubjectDropdown/> <LevelDropdown/>
+
+    </div>
+  )
+}
--- src/app/components/cards/Card.tsx
+++ src/app/components/cards/Card.tsx
@@ -1,66 +1,88 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { useAnimation, motion } from "framer-motion";
 import { LiaChalkboardTeacherSolid } from "react-icons/lia";
 import { PiStudentDuotone } from "react-icons/pi";
 import { IoSearchSharp } from "react-icons/io5";
 
 export default function Home() {
-  const cardItems =  [
-          {
-            icon: <LiaChalkboardTeacherSolid className="w-8 h-8 " />,
-            title: "Teacher Moderation",
-            desc: `Module leaders review and approve submissions to ensure they align
-                   with course objectives, maintain academic accuracy, and provide a
-                   trusted learning experience for all students.`,
-          },
-          {
-            icon: <PiStudentDuotone className="w-8 h-8" />,
-            title: "Student Contributions",
-            desc: `Students submit tutorials, articles, or GitHub projects supporting
-                   their coursework, helping peers learn from trusted resources.`,
-          },
-          {
-            icon: <IoSearchSharp className="w-7 h-7" />,
-            title: "Tag-Based Discovery",
-            desc: `Resources use smart tags by course week, topic, and contributor,
-                   making it easy to quickly filter and find what you need.`,
-          },
-        ]
+
+  const cardItems = [
+    {
+      icon: <LiaChalkboardTeacherSolid className="w-8 h-8" />,
+      title: "Teacher Moderation",
+      desc: `Module leaders review and approve submissions to ensure they align
+             with course objectives, maintain academic accuracy, and provide a
+             trusted learning experience for all students.`,
+    },
+    {
+      icon: <PiStudentDuotone className="w-8 h-8" />,
+      title: "Student Contributions",
+      desc: `Students submit tutorials, articles, or GitHub projects supporting
+             their coursework, helping peers learn from trusted resources.`,
+    },
+    {
+      icon: <IoSearchSharp className="w-7 h-7" />,
+      title: "Tag-Based Discovery",
+      desc: `Resources use smart tags by course week, topic, and contributor,
+             making it easy to quickly filter and find what you need.`,
+    },
+  ];
 
   return (
     <div className="flex items-center justify-center">
       <div className="max-w-6xl mx-auto flex flex-wrap justify-center gap-8 -mt-50">
-       {cardItems.map((card, index) => (
-          <motion.div
-            key={index}
-            className="w-[320px] h-[350px] bg-white border rounded-xl p-6 shadow-[6px_6px_0px_#000000]   hover:shadow-[6px_6px_0px_#000000] transition"
-            whileHover={{
-              scale: 1.04,
-              rotate: 0.5,
-              transition: {
-                duration: 0.3,
-                ease: "easeInOut",
-              },
-            }}
-          >
-            <div className="mb-3 border-none bg-[#E3E3E3] rounded-4xl h-10 w-10 p-1 ">{card.icon}</div>
-            <motion.h4
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.25 }}
-              className="text-[30px] font-semibold mb-2 origin-left"
+        {cardItems.map((card, index) => {
+          const controls = useAnimation();
+
+          return (
+            <motion.div
+              key={index}
+              className="w-[320px] h-[350px] bg-white border rounded-xl p-6 shadow-[8px_8px_0px_#000,9.5px_9.5px_0px_#ffffff] cursor-pointer"
+              initial={{ scale: 1 }}
+              animate={controls}
+              style={{ transformOrigin: "top left" }}
+              onHoverStart={() => {
+                controls.start({
+                  scale: [1, 1.05, 1.025, 1.045, 1.03],
+                  transition: {
+                    type: "tween",
+                    duration: 0.6,
+                    ease: "easeOut",
+                  },
+                });
+              }}
+              onHoverEnd={async () => {
+                await controls.start({
+                  scale: 1,
+                  transition: { duration: 0.1, ease: "easeInOut" },
+                });
+                await controls.start({
+                  scale: [1, 1.02, 1.01, 1.02, 1.01],
+                  transition: { duration: 0.6, ease: "easeOut" },
+                });
+              }}
             >
-              {card.title}
-            </motion.h4>
-            <motion.p
-              whileHover={{ scale: 1.02 }}
-              transition={{ duration: 0.2 }}
-              className="text-[15px]/6 text-gray-700 origin-left"
-            >
-              {card.desc}
-            </motion.p>
-          </motion.div>
-        ))}
+              <div className="mb-3 border-none bg-[#E3E3E3] rounded-4xl h-10 w-10 p-1">
+                {card.icon}
+              </div>
+              <motion.h4
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.25 }}
+                className="text-[30px] font-semibold mb-2 origin-left"
+              >
+                {card.title}
+              </motion.h4>
+              <motion.p
+                whileHover={{ scale: 1.02 }}
+                transition={{ duration: 0.2 }}
+                className="text-[15px]/6 text-gray-700 origin-left"
+              >
+                {card.desc}
+              </motion.p>
+            </motion.div>
+          );
+        })}
       </div>
     </div>
   );
